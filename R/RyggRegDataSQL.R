8fa36d1b5c6764fa72b22c25cf9f6428a2fc8a97
Olsen Lena Ringstad <lro2402unn@hn.helsenord.no>
2017-01-23 13:10:43 +0100

    Data t.o.m. 2011

--- R/RyggRegDataSQL.R
+++ R/RyggRegDataSQL.R
@@ -161,7 +161,7 @@ RyggRegDataSQL <- function() {
 	Utfylt3Mnd,
 	Vekt
 FROM Uttrekk_Rapport
-  WHERE (OpAar<2016) ') #
+  WHERE (OpAar<2012) ') #
 #                  WHERE OpDato >= \'', datoFra, '\' AND OpDato <= \'', datoTil, '\'')
 
 RegData <- rapbase::LoadRegData(registryName, query, dbType)
